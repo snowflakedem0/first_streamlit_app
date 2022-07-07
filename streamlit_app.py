232ccc72fb9c055f4be753baab5be9d89015e1a3
snowflakedem0 <108901193+snowflakedem0@users.noreply.github.com>
2022-07-08 02:41:23 +0530

    Update streamlit_app.py
    Added additional messages

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,9 @@ import streamlit
 
 streamlit.title('My Snowflake Learning Path')
 
-streamlit.header('Completed Datawarehouse essentials')
+streamlit.header('🥣 Completed Datawarehouse essentials')
+streamlit.text('🥗 Completed Level Up: First Concepts')
+streamlit.text('🐔 Level Up: Snowflake Key Concepts')
+streamlit.text('🥑 Level Up: Snowflake Ecosystem')
+
+streamlit.header('🍞 Data Application Builders Workshop in progress')
