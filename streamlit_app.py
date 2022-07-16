d25ec02518585fe8f0a922deb1f2be78a22c7676
snowflakedem0 <108901193+snowflakedem0@users.noreply.github.com>
2022-07-16 23:33:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,18 +58,24 @@ except URLError as e:
 streamlit.stop()
 
 #import snowflake.connector
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-#my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_cur.execute("select * from fruit_load_list")
-my_data_rows = my_cur.fetchall()
-#streamlit.text("Hello from Snowflake:")
 streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+#Snowflake-related functions
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("select * from fruit_load_list")
+    return my_cur.fetchall()
+
+# Add a buttom to load the fruit
+if streamlit.button('Get Fruit Load list'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_rows = get_fruit_load_list()
+  streamlit.dataframe(my_data_rows)
+  
 
 
-add_my_fruit = streamlit.text_input('What fruit would you like information about?','jackfruit')
-streamlit.write('Thanks for adding ', add_my_fruit)
 
-# Test case
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+# add_my_fruit = streamlit.text_input('What fruit would you like information about?','jackfruit')
+# streamlit.write('Thanks for adding ', add_my_fruit)
+
+# # Test case
+# my_cur.execute("insert into fruit_load_list values ('from streamlit')")
